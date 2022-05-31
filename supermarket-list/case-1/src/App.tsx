2eaac603d118e0a7c5cc8a83f091b0a087e21bf7
juanga13 <juangabrielricci@sirius.com.ar>
2022-05-31 12:40:48 -0300

    feat: supermarket-list case 1 (creo que me pase lmao)

--- supermarket-list/case-1/src/App.tsx
+++ supermarket-list/case-1/src/App.tsx
@@ -11,6 +11,8 @@ interface Form extends HTMLFormElement {
 
 function App() {
   const [items, setItems] = useState<Item[]>([]);
+  const [value, setValue] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   function handleToggle(id: Item["id"]) {
     setItems((items) =>
@@ -19,34 +21,51 @@ function App() {
   }
 
   function handleAdd(event: React.ChangeEvent<Form>) {
-    // Should implement
+    event.preventDefault();
+    setLoading(true);
+    api.add(value).then((items) => {
+      setItems(items);
+      setLoading(false);
+    });
+    setValue("");
   }
 
   function handleRemove(id: Item["id"]) {
-    setItems((items) => items.filter((item) => item.id !== id));
+    setLoading(true);
+    api.remove(id).then((items) => {
+      setItems(items);
+      setLoading(false);
+    });
   }
 
   useEffect(() => {
-    api.list().then(setItems);
+    setLoading(true);
+    api.list().then((items) => {
+      setItems(items);
+      setLoading(false);
+    });
   }, []);
 
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form onSubmit={handleAdd}>
-        <input name="text" type="text" />
-        <button>Add</button>
+        <input name="text" type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+        <button type="submit">Add</button>
       </form>
+      {loading && <p>loading...</p>}
       <ul>
-        {items?.map((item) => (
-          <li
-            key={item.id}
-            className={item.completed ? styles.completed : ""}
-            onClick={() => handleToggle(item.id)}
-          >
-            {item.text} <button onClick={() => handleRemove(item.id)}>[X]</button>
-          </li>
-        ))}
+        {items
+          .sort((a, b) => a.id - b.id)
+          .map((item) => (
+            <li
+              key={item.id}
+              className={item.completed ? styles.completed : ""}
+              onClick={() => handleToggle(item.id)}
+            >
+              {item.text} <button onClick={() => handleRemove(item.id)}>[X]</button>
+            </li>
+          ))}
       </ul>
     </main>
   );
--- supermarket-list/case-1/src/api.ts
+++ supermarket-list/case-1/src/api.ts
@@ -4,23 +4,72 @@ export default {
   list: (): Promise<Item[]> =>
     new Promise((resolve) =>
       setTimeout(() => {
-        resolve([
-          {
-            id: 1,
-            text: "Some thing to buy",
-            completed: false,
-          },
-          {
-            id: 2,
-            text: "Some other thing to buy",
-            completed: true,
-          },
-          {
-            id: 3,
-            text: "Some last to buy",
-            completed: false,
-          },
-        ]);
+        const storedList = localStorage.getItem("items");
+        let list = null;
+
+        if (storedList) {
+          list = JSON.parse(storedList);
+        } else {
+          list = [
+            {
+              id: 1,
+              text: "Some thing to buy",
+              completed: false,
+            },
+            {
+              id: 2,
+              text: "Some other thing to buy",
+              completed: true,
+            },
+            {
+              id: 3,
+              text: "Some last to buy",
+              completed: false,
+            },
+          ];
+          localStorage.setItem("lastIndex", "3");
+        }
+
+        localStorage.setItem("items", JSON.stringify(list));
+
+        resolve(list);
+      }, 1000),
+    ),
+  remove: (itemId: number): Promise<Item[]> =>
+    new Promise((resolve) =>
+      setTimeout(() => {
+        const storedList = localStorage.getItem("items");
+
+        const list: Item[] = storedList ? JSON.parse(storedList) : [];
+
+        const newList = list.filter((item) => item.id !== itemId);
+
+        localStorage.setItem("items", JSON.stringify(newList));
+        resolve(newList);
+      }, 1000),
+    ),
+  add: (itemText: string): Promise<Item[]> =>
+    new Promise((resolve) =>
+      setTimeout(() => {
+        const storedList = localStorage.getItem("items");
+        const storedLastIndex = localStorage.getItem("lastIndex");
+
+        const list = storedList ? JSON.parse(storedList) : [];
+        const lastIndex = storedLastIndex ? parseInt(storedLastIndex) : 1;
+
+        const item: Item = {
+          id: lastIndex + 1,
+          completed: false,
+          text: itemText,
+        };
+
+        localStorage.setItem("lastIndex", (lastIndex + 1).toString());
+
+        const newList = [...list, item];
+
+        localStorage.setItem("items", JSON.stringify(newList));
+
+        resolve(newList);
       }, 1000),
     ),
 };
